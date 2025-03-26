9dd6d084c68a35559884c841a9c37e6b6c3cb60b
Rey Dominic Tome <reydominictome@gmail.com>
2025-03-26 19:43:25 +0800

    fix 🔧 build issue

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -4,7 +4,6 @@ import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import { SmtpMessage } from "../smtp-message";
 import { useAuthActions } from "@/hooks/use-auth-actions";
 
 export default function Signup() {
@@ -47,7 +46,6 @@ export default function Signup() {
           <SubmitButton pendingText="Signing up...">Sign up</SubmitButton>
         </div>
       </form>
-      <SmtpMessage />
     </>
   );
 }
--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -10,20 +10,9 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Laptop, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 const ThemeSwitcher = () => {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  // useEffect only runs on the client, so now we can safely show the UI
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   const ICON_SIZE = 16;
 
